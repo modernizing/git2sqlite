e24f9447bfb3f901ae84e8e066ce508918d296ab
Phodal Huang <h@phodal.com>
2021-02-03 09:49:48 +0800

    feat: add rusqlite offical demo code for testing

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,15 @@ pub mod git_command;
 pub mod git_log_parser;
 pub mod coco_commit;
 
+use rusqlite::{params, Connection, Result};
+
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
 pub fn analysis(local_path: &Path) -> Vec<CocoCommit> {
     let messages = get_commit_message(Some(format!("{}", local_path.display())));
     let vec = GitMessageParser::parse(messages.as_str());
@@ -18,9 +27,41 @@ pub fn analysis(local_path: &Path) -> Vec<CocoCommit> {
     return vec;
 }
 
+fn main() -> Result<()> {
+    // let path = Path::new(".");
+    // let results = analysis(path);
+    // println!("{:?}", results);
 
-fn main() {
-    let path = Path::new(".");
-    let results = analysis(path);
-    println!("{:?}", results);
+    let conn = Connection::open_in_memory()?;
+    conn.execute(
+        "CREATE TABLE person (
+                  id              INTEGER PRIMARY KEY,
+                  name            TEXT NOT NULL,
+                  data            BLOB
+                  )",
+        params![],
+    )?;
+    let me = Person {
+        id: 0,
+        name: "Steven".to_string(),
+        data: None,
+    };
+    conn.execute(
+        "INSERT INTO person (name, data) VALUES (?1, ?2)",
+        params![me.name, me.data],
+    )?;
+
+    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
+    let person_iter = stmt.query_map(params![], |row| {
+        Ok(Person {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            data: row.get(2)?,
+        })
+    })?;
+
+    for person in person_iter {
+        println!("Found person {:?}", person.unwrap());
+    }
+    Ok(())
 }
