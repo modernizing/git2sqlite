f1dda4302564619238125155b0804b1f9eaf1cc8
Phodal Huang <h@phodal.com>
2021-02-03 10:08:43 +0800

    feat: init sqlite schema

--- src/main.rs
+++ src/main.rs
@@ -32,36 +32,56 @@ fn main() -> Result<()> {
     // let results = analysis(path);
     // println!("{:?}", results);
 
-    let conn = Connection::open_in_memory()?;
+    let conn = Connection::open("coco_git.db")?;
     conn.execute(
-        "CREATE TABLE person (
+        "CREATE TABLE IF NOT EXISTS git_commit (
                   id              INTEGER PRIMARY KEY,
-                  name            TEXT NOT NULL,
-                  data            BLOB
+                  branch          TEXT,
+                  commit_id       TEXT,
+                  author          TEXT,
+                  committer       TEXT,
+                  date            INT,
+                  message         TEXT,
+                  parent_hashes   TEXT,
+                  tree_hash       TEXT
                   )",
         params![],
     )?;
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None,
-    };
+
     conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        params![me.name, me.data],
+        "CREATE TABLE IF NOT EXISTS file_changed (
+                  id              INTEGER PRIMARY KEY,
+                  commit_id       INTEGER,
+                  added           INTEGER,
+                  deleted         INTEGER,
+                  file            TEXT,
+                  mode            TEXT,
+                  FOREIGN KEY (commit_id) REFERENCES git_commit
+                  )",
+        params![],
     )?;
 
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-    let person_iter = stmt.query_map(params![], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
+    // let me = Person {
+    //     id: 0,
+    //     name: "Steven".to_string(),
+    //     data: None,
+    // };
+    // conn.execute(
+    //     "INSERT INTO person (name, data) VALUES (?1, ?2)",
+    //     params![me.name, me.data],
+    // )?;
+    //
+    // let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
+    // let person_iter = stmt.query_map(params![], |row| {
+    //     Ok(Person {
+    //         id: row.get(0)?,
+    //         name: row.get(1)?,
+    //         data: row.get(2)?,
+    //     })
+    // })?;
+    //
+    // for person in person_iter {
+    //     println!("Found person {:?}", person.unwrap());
+    // }
     Ok(())
 }
